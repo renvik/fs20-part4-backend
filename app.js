ed9fcbc273dfe59ddfb9e69c858e67ea32ac47f9
Renvik Reko-Aleksi <renvik@cubbli20-gold.local>
2021-02-10 09:39:21 +0200

    4.2 done

--- app.js
+++ app.js
@@ -19,11 +19,13 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 
 
 app.use(cors())
-app.use(express.static('build'))
+app.use(express.static('build')) // express' built-in middleware
 app.use(express.json())
+app.use(middleware.requestLogger)
+app.use('/api/blogs', blogsRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 app.use(middleware.requestLogger)
-app.use('api/blogs', blogsRouter)
+
 
 module.exports = app
