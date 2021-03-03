a4862164aed29950ebfdea79fe7e32bf656d90ff
Renvik Reko-Aleksi <renvik@cubbli20-gold.local>
2021-03-03 14:01:38 +0200

    exercises 4.4, 4.5, 4.8

--- tests/dummy.test.js
+++ tests/dummy.test.js
@@ -1,8 +1,11 @@
+//imports list_helper:
 const listHelper = require('../utils/list_helper')
-
+// first test suite:
 test('dummy returns one', () => {
   const blogs = []
 
   const result = listHelper.dummy(blogs)
+  //jest-method "expect" wraps value to object and then uses matcher toBe
+  //defines the expected result of the test
   expect(result).toBe(1)
 })
