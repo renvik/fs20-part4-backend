482a6caa1011b0659890d4f01ad838aaac895656
Renvik Reko-Aleksi <renvik@cubbli20-gold.local>
2021-03-17 15:18:20 +0200

    finished exercises in 4B

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,4 +1,4 @@
-// place for routes, notesRouter (a router object) exports router, all routes are connected to notesRouter-object
+// place for routes, blogsRouter (a router object) exports router, all routes are connected to the blogsRouter-object
 
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
@@ -18,6 +18,29 @@ blogsRouter.post('/', async (request, response) => {
     const blog = new Blog(request.body).save()
     response.status(200).json(blog)
   }
+})
+// route that enables deleting a blog
+blogsRouter.delete('/:id', async (request, response) => {
+  // response consists of all the info that the request has, for instance idcreates id variable which consists of
+  // uses mongooses model and findByIdAndDelete-function
+  await Blog.findByIdAndDelete(request.params.id)
+  // response is how the request is responded (both succesfully deleted and failed):. In theory response's object status-method sends code 204
+  response.status(204).end()
+})
+// route that enable updating blog's properties
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+  // uses mongooses model and findByIdAndUpdate-function
+  await Blog.findByIdAndUpdate(request.params.id, blog, { new : true })
+  // response is how the request is responded (both succesfully deleted and failed):. In theory response's object status-method sends code 204
+  response.status(204).end()
 })
 
 module.exports = blogsRouter
